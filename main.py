6db7a000db1f8458ac61cff70018f8f9b08e7930
Llelepipede <pchesneau3103@gmail.com>
2020-05-18 22:14:50 +0200

    avec liste des choses a faire

--- main.py
+++ main.py
@@ -1,4 +1,67 @@
+
+
+# definir un univers
+# menu
+    # 2 mode de jeu
+        # ia
+        # jeu en local
+# ecran jeu
+    # 2 camp
+        # un joueur
+            # vie ( 100 max )
+            # bouclier( 30 max )
+        # des ressources ( 3 types )
+            # bois
+            # nouritures
+            # pierres
+        # des fournisseurs de ressources
+        # une main
+            # 7 cartes
+                # un nom
+                # un cout
+                    # 0 si fournisseur de ressources ( arbitraire pour l'instant )
+                # un effet
+                    # soin
+                    # dammage
+                    # pioche
+                    # ...
+                # des valeurs
+                    # ???
+                # une cible
+        # un deck
+            # pioche aléatoire
+        # defausse
+
+    # tour a tour
+        # +1 par fournisseur dans la ressource en question
+        # une pioche ( si carte < 7 )
+            # pioche aléatoire
+        # auto deffausse si > 7 cartes en main
+        # jouer une carte si les ressources le permettent
+
+    # historique des tours
+
+    # fin partie si joueur a pv <= pv
+# ecran victoire
+    # stats de la partie
+# ecran tuto
+    # explique comment on joue
+# ecran option
+    # créer
+    # consulter
+    # supprimer
+    # modifier
+    # sauvegarde des cartes deja créer
+
+
+
+
+
 #v test de home page v#
+
+
+
+
 
 #v les includes v#
 from math import *
@@ -34,11 +97,12 @@ def create_windows():
 
 
 
+
 def boucle_jeu():
     end = 0
     while not end:
         for event in pygame.event.get():
-            #print(event)
+            print(event)
             if event.type == pygame.QUIT:
                 end = 1
 
